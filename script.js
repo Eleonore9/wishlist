fef121373120cda63a52ebe54d244029fc830615
Eleonore9 <eleonore.mayola@ymail.com>
2014-01-16 00:34:31 +0100

    trying to change item label from pending to completed

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 /* Exercise 1: Wish list */
 $(document).ready(function() {
+	//Add item in text field to the list on button click
   $("button#add-to-list").on('click', function() {
-  	$item = $("#item").val()
+  	var $item = $("#item").val();
   	addToList($item);
-  	$("#item").val("")
-  	$("#item").focus()
-  })
+  	$("#item").val(""); //Clear text field
+  	$("#item").focus(); //Cursor back to text field
+  });
+  //Add item in text field to the list on button click
+  $('span.label.pending').on('click', function() {
+  	console.log("span click!");
+  	parent_node = $(this).parent();
+  	parent_node.addClass(".completed");
+  });
 });
 
-
 function addToList(item){
-	$("ol#items").append("<li>" + item + "</li>")
+	//Takes in an argument and appends it to the list in a li tag with the 'pending' label
+	$("ol#items").append("<li>" + item + "<span class='label pending'>Pending</span>" + "</li>");
 }
-
