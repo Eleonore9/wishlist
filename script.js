c410ea341b839d4bbaa929afe9b79a10e6fb506b
Eleonore9 <eleonore.mayola@ymail.com>
2014-01-22 22:16:57 +0100

    I can finally change css class of element added

--- script.js
+++ script.js
@@ -7,15 +7,19 @@ $(document).ready(function() {
   	$("#item").val(""); //Clear text field
   	$("#item").focus(); //Cursor back to text field
   });
-  //Add item in text field to the list on button click
-  $('span.label.pending').on('click', function() {
-  	console.log("span click!");
-  	parent_node = $(this).parent();
-  	parent_node.addClass(".completed");
-  });
+  //Listen for click event on the page
+  $(document).on('click', '.pending', function() {
+  	//Add item in text field to the list on button click
+  	$("span.label.pending").on('click', function() {
+  		parent_node = $(this).parent();
+  		parent_node.addClass("completed");
+		parent_node.remove();
+  	});
+  })
+
 });
 
 function addToList(item){
-	//Takes in an argument and appends it to the list in a li tag with the 'pending' label
+	//Takes in an element and appends it to the list in a li tag with the 'pending' label
 	$("ol#items").append("<li>" + item + "<span class='label pending'>Pending</span>" + "</li>");
-}
+};
